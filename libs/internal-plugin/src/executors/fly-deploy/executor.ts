30ebc20fbc81b0ba49a23f71f8d19a4a54b710dd
ilMike42 <ilmike_others@icloud.com>
2023-11-28 09:52:03 +0100

    improved fly deployment and added angular frontend config

--- libs/internal-plugin/src/executors/fly-deploy/executor.ts
+++ libs/internal-plugin/src/executors/fly-deploy/executor.ts
@@ -6,11 +6,11 @@ export default async function runExecutor(
 ) {
   const cwd = options.distLocation;
   const results = execSync(`flyctl apps list`);
-  if (results.toString().includes(options.flyAppName)) {    
-    execSync(`flyctl deploy -c src/fly.toml`, { cwd });
+  if (results.toString().includes(options.flyAppName)) {        
+    execSync(`flyctl deploy`, { cwd });
   } else {
     // consult https://fly.io/docs/reference/regions/ to get best region for you
-    execSync(`flyctl launch --now --name=${options.flyAppName} --region=mad`, {
+    execSync(`flyctl launch --now --name=${options.flyAppName} --region=mad --vm-memory=256`, {
       cwd,
     });
   }

ee5fe0856b2d274c47f1cd034e1a736fbc0ac2a2
ilMike42 <ilmike_others@icloud.com>
2023-11-29 02:05:35 +0100

    added theme selector to header

--- libs/shared-ui/src/components/header/header.component.ts
+++ libs/shared-ui/src/components/header/header.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { ThemeSelectorComponent } from '../theme-selector/theme-selector.component';
 
 @Component({
   selector: 'portfolio-full-stack-monorepo-header',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, ThemeSelectorComponent],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
 })

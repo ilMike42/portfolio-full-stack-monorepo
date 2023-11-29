010f735c6996c94924f52fb278d68915d3bb6004
ilMike42 <ilmike_others@icloud.com>
2023-11-29 01:16:18 +0100

    added theme  handling in app component TODO: document the feature

--- apps/angular-frontend/src/app/app.component.ts
+++ apps/angular-frontend/src/app/app.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject, OnInit, effect, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { DaisyUiThemeService, FooterComponent, HeaderComponent } from '@portfolio-full-stack-monorepo/shared-ui';
 
 @Component({
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, HeaderComponent, FooterComponent],
   selector: 'portfolio-full-stack-monorepo-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'angular-frontend';
+  
+  themeService = inject(DaisyUiThemeService)
+
+  public constructor(@Inject(DOCUMENT) doc: Document) {
+    // todo: document this
+    effect(() => {
+      doc.documentElement.setAttribute('data-theme', this.themeService.theme());
+    })
+  }
+
 }

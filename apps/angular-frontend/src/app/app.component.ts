b5bf2f075601de793e1597145d2b02a00e363137
ilMike42 <ilmike_others@icloud.com>
2023-11-30 20:12:43 +0100

    added documentation for theme handling

--- apps/angular-frontend/src/app/app.component.ts
+++ apps/angular-frontend/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { DOCUMENT } from '@angular/common';
 import { Component, Inject, effect, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { DaisyUiThemeService, FooterComponent, HeaderComponent } from '@portfolio-full-stack-monorepo/shared-ui';
+import { environment } from '../environments/environment';
 
 @Component({
   standalone: true,
@@ -12,14 +13,22 @@ import { DaisyUiThemeService, FooterComponent, HeaderComponent } from '@portfoli
 })
 export class AppComponent {
   title = 'angular-frontend';
-  
+
   themeService = inject(DaisyUiThemeService)
 
+  /**
+   * 
+   * @param doc 
+   * The constructor of this app.component sets the data-theme attribute
+   * on the <html> tag, this handles the daisyUI theme using the DaisyUI service.s
+   */
   public constructor(@Inject(DOCUMENT) doc: Document) {
-    // todo: document this
     effect(() => {
       doc.documentElement.setAttribute('data-theme', this.themeService.theme());
     })
+
+    console.log(environment.BASE_URL);
+    
   }
 
 }

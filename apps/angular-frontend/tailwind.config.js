d62d41d38f5eed9a7e149f55403c57dfcf08a88e
ilMike42 <ilmike_others@icloud.com>
2023-11-30 23:15:15 +0100

    added themes

--- apps/angular-frontend/tailwind.config.js
+++ apps/angular-frontend/tailwind.config.js
@@ -14,6 +14,6 @@ module.exports = {
     require("daisyui")
   ],
   daisyui: {
-    themes: ["light", "dark", "cupcake", "retro", "cyberpunk"],
+    themes: ["light", "dark", "cupcake", "retro", "cyberpunk", "synthwave", "night"],
   },
 };

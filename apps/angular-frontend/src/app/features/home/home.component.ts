431bb41b2223de9ba4c1240ef65e33b4eaf1221a
ilMike42 <ilmike_others@icloud.com>
2023-11-30 20:31:23 +0100

    updated home

--- apps/angular-frontend/src/app/features/home/home.component.ts
+++ apps/angular-frontend/src/app/features/home/home.component.ts
@@ -1,22 +1,13 @@
-import { Component, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { DaisyUITheme, DaisyUiThemeService } from '@portfolio-full-stack-monorepo/shared-ui';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'portfolio-full-stack-monorepo-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
-  themeService = inject(DaisyUiThemeService)
-
-  updateTheme() {    
-    if(this.themeService.theme() === DaisyUITheme.dark) {
-      this.themeService.setTheme(DaisyUITheme.cupcake)
-    } else {
-      this.themeService.setTheme(DaisyUITheme.dark)
-    }
-  }
 }

80110d08530b57d633e38540272d95079592a3d1
ilMike42 <ilmike_others@icloud.com>
2023-12-07 00:35:28 +0100

    refactored api to read from a view instead of the collections

--- apps/fastify-backend/src/app/controllers/timeline.ts
+++ apps/fastify-backend/src/app/controllers/timeline.ts
@@ -1,42 +1,8 @@
 import { FastifyRequest } from "fastify";
 
 const getTimelineController = async (req: FastifyRequest) => {
-
-  // TODO: refactor this into a MongoDB view
-
-
-  const workplacesCollection = req.server.mongo.db.collection('Workplaces');
-  const educationCollection = req.server.mongo.db.collection('Education');
-  const personalExperiencesCollection = req.server.mongo.db.collection('PersonalExperiences');
-
-  const workplaces = await workplacesCollection.find().sort('year', -1).toArray();
-  const education = await educationCollection.find().sort('year', -1).toArray();
-  const personalExperiences = await personalExperiencesCollection.find().sort('year', -1).toArray();
-  return [
-    {
-      title: "Experiences",
-      subSections: [
-        {
-          tags: ['Workplace','Softfobia'],
-          items: workplaces
-        },
-        {
-          tags: ['Personal', 'Hobbies'],
-          items: personalExperiences
-        }
-      ],
-
-    },
-    {
-      title: "Education",
-      subSections: [
-        {
-          tags: [],
-          items: education
-        }
-      ]
-    },
-  ];
+  const view = req.server.mongo.db.collection('SectionsView');
+  return await view.find().toArray();
 }
 
 export { getTimelineController }

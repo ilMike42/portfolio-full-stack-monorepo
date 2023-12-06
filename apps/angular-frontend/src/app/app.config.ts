a24141e7aee3051c5e33e1f4252adc1b3ad700a7
ilMike42 <ilmike_others@icloud.com>
2023-12-06 01:13:10 +0100

    fixed http client provider

--- apps/angular-frontend/src/app/app.config.ts
+++ apps/angular-frontend/src/app/app.config.ts
@@ -1,8 +1,8 @@
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(appRoutes), importProvidersFrom(HttpClientModule)],
+  providers: [provideRouter(appRoutes), provideHttpClient()],
 };

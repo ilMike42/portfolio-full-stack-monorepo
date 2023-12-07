d55f9d226cc62f530e4c6a739d619fd257b9cdcd
ilMike42 <ilmike_others@icloud.com>
2023-12-07 01:31:05 +0100

    timeline component: added placeholder for loading state

--- apps/angular-frontend/src/app/features/timeline/timeline.component.ts
+++ apps/angular-frontend/src/app/features/timeline/timeline.component.ts
@@ -2,11 +2,12 @@ import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ThemeSelectorComponent } from '@portfolio-full-stack-monorepo/shared-ui';
 import { TimelineService } from './services/timeline.service';
+import { TimelinePlaceholderComponent } from './components/timeline-placeholder/timeline-placeholder.component';
 
 @Component({
   selector: 'portfolio-full-stack-monorepo-timeline',
   standalone: true,
-  imports: [CommonModule, ThemeSelectorComponent],
+  imports: [CommonModule, ThemeSelectorComponent, TimelinePlaceholderComponent],
   templateUrl: './timeline.component.html',
   styleUrls: ['./timeline.component.css'],
 })
